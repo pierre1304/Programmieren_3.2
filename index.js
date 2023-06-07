76a948c9d43a36e100cadfda0a2d2f8f310baeeb
pierre1304 <piere.raffel@gmail.com>
2023-06-07 19:44:16 +0200

    server version

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const app = express();
 /*app.get("/name/:name", function(req, res){
     let name = req.params.name;
     res.send("<h1>hello " + name +"</h1>");
-});*/
+});
 
 app.get("/website/:website", function(req, res){
     let website = req.params.website;
     res.redirect("https://www.google.com/search?q=" + website)
-});
+});*/
+
+app.use(express.static('./GameOfLife'))
+
+app.get("/GameOfLife", function(req, res){
+    res.redirect("/index.html");
+})
 
 app.listen(3000, function(){
     console.log("test");
